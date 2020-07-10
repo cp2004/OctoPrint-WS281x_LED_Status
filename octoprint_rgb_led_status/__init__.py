954f8d58dc72e3f012b6e84bce889ec668facf4f
cp2004 <31997505+cp2004@users.noreply.github.com>
2020-07-10 16:45:18 +0100

    :boom: Not currently Python 2 compatible

--- octoprint_rgb_led_status/__init__.py
+++ octoprint_rgb_led_status/__init__.py
@@ -301,8 +301,8 @@ __plugin_name__ = "RGB LED Status"
 # Python 2. New plugins should make sure to run under both versions for now. Uncomment one of the following
 # compatibility flags according to what Python versions your plugin supports!
 # __plugin_pythoncompat__ = ">=2.7,<3" # only python 2
-# __plugin_pythoncompat__ = ">=3,<4" # only python 3
-__plugin_pythoncompat__ = ">=2.7,<4" # python 2 and 3
+__plugin_pythoncompat__ = ">=3,<4" # only python 3
+# __plugin_pythoncompat__ = ">=2.7,<4" # python 2 and 3
 
 
 def __plugin_load__():
