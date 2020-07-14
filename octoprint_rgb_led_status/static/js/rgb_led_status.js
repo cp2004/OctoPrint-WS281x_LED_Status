1c03ef4fed2752114d1f8ebd9d2aced59a6a32ef
cp2004 <31997505+cp2004@users.noreply.github.com>
2020-07-14 11:34:31 +0100

    :recycle: Remove viewmodel skeleton from js

--- octoprint_rgb_led_status/static/js/rgb_led_status.js
+++ octoprint_rgb_led_status/static/js/rgb_led_status.js
@@ -76,28 +76,6 @@ $(function() {
         dependencies: ['wizardViewModel'],
         elements: ['#wizard_plugin_egb_led_status']
     });
-
-    function Rgb_led_statusViewModel(parameters) {
-        var self = this;
-
-        // assign the injected parameters, e.g.:
-        // self.loginStateViewModel = parameters[0];
-        // self.settingsViewModel = parameters[1];
-
-        // TODO: Implement your plugin's view model here.
-    }
-
-    /* view model class, parameters for constructor, container to bind to
-     * Please see http://docs.octoprint.org/en/master/plugins/viewmodels.html#registering-custom-viewmodels for more details
-     * and a full list of the available options.
-     */
-    OCTOPRINT_VIEWMODELS.push({
-        construct: Rgb_led_statusViewModel,
-        // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
-        dependencies: [ /* "loginStateViewModel", "settingsViewModel" */ ],
-        // Elements to bind to, e.g. #settings_plugin_rgb_led_status, #tab_plugin_rgb_led_status, ...
-        elements: [ /* ... */ ]
-    });
 });
 
 
