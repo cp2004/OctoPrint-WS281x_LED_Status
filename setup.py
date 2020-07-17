d716ba2be307b5ac28bb8c64dc12a748395b9fc2
cp2004 <31997505+cp2004@users.noreply.github.com>
2020-07-17 23:26:48 +0100

    :pencil2: Update url in setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ plugin_author = "Charlie Powell"
 plugin_author_email = "charlie.powell@gmail.com"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/cp2004/OctoPrint-RGB_LED_Status"
+plugin_url = "https://github.com/cp2004/OctoPrint-WS281x_LED_Status"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
