b29149bb2ee7b5936741e516224303196c49babb
11chrisadams11 <11chrisadams11@gmail.com>
2020-10-24 14:28:26 -0600

    Add ability to reverse the progress effect.
    Added reverse argument and code to display the progress effect in the opposite order.

--- octoprint_ws281x_led_status/effects/progress.py
+++ octoprint_ws281x_led_status/effects/progress.py
@@ -6,19 +6,23 @@ import time
 from octoprint_ws281x_led_status.util import blend_two_colors
 
 
-def progress(strip, queue, value, progress_color, base_color, max_brightness=255):
+def progress(strip, queue, value, progress_color, base_color, max_brightness=255, reverse=False):
     strip.setBrightness(max_brightness)
     num_pixels = strip.numPixels()
     upper_bar = (value / 100) * num_pixels
     upper_remainder, upper_whole = math.modf(upper_bar)
-    lower_base = ((100 - value) / 100) * num_pixels
-    lower_remainder, lower_whole = math.modf(lower_base)
+    pixels_remaining = num_pixels
     for i in range(int(upper_whole)):
-        strip.setPixelColorRGB(i, *progress_color)
-    if upper_remainder:
+        pixel = ((num_pixels - 1) - i) if reverse else i
+        strip.setPixelColorRGB(pixel, *progress_color)
+        pixels_remaining -= 1
+    if upper_remainder > 0.0:
         tween_color = blend_two_colors(progress_color, base_color, upper_remainder)
-        strip.setPixelColorRGB(int(upper_whole), *tween_color)
-    for i in range(int(lower_whole)):
-        strip.setPixelColorRGB(((num_pixels - 1) - i), *base_color)
+        pixel = ((num_pixels - int(upper_whole)) - 1) if reverse else int(upper_whole)
+        strip.setPixelColorRGB(pixel, *tween_color)
+        pixels_remaining -= 1
+    for i in range(pixels_remaining):
+        pixel = ((pixels_remaining - 1) - i) if reverse else ((num_pixels - pixels_remaining) + i)
+        strip.setPixelColorRGB(pixel, *base_color)
     strip.show()
     time.sleep(0.1)
