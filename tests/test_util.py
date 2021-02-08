be90b3fe4b0d228f2c07e802b22159c361891f30
cp2004 <31997505+cp2004@users.noreply.github.com>
2021-02-08 20:17:40 +0000

    :green_heart: Add tests for #95, blend_two_colors

--- tests/test_util.py
+++ tests/test_util.py
@@ -99,3 +99,22 @@ class WS281xUtilTestCase(unittest.TestCase):
             mock_popen.communicate.assert_called_once_with(called_once_with)
         else:
             mock_popen.communicate.assert_called_once_with()
+
+    def test_blend_colors(self):
+        from octoprint_ws281x_led_status.util import blend_two_colors
+
+        # Test value > 100
+        result = blend_two_colors((255, 0, 0), (0, 0, 255), float(101) / 100)
+        self.assertTupleEqual((128, 0, 0), result)
+
+        # Test colour > 255
+        result = blend_two_colors((187, 245, 2), (345, 255, 255), float(101) / 100)
+        self.assertTupleEqual((92, 122, 0), result)
+
+        # Test value = 0
+        result = blend_two_colors((187, 245, 2), (156, 0, 255), float(0) / 100)
+        self.assertTupleEqual((171, 122, 128), result)
+
+        # Test with nothing special
+        result = blend_two_colors((187, 245, 6), (100, 0, 255), float(45) / 100)
+        self.assertTupleEqual((69, 55, 71), result)
