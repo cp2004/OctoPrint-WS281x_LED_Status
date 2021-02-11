cp2004/OctoPrint-WS281x_LED_Status

commit 95ea39564fb89e12a7195d211f31fdaeca4a4447

:recycle: Rework effects to use queues, fix issues with fadein/out

octoprint_ws281x_led_status/effects/standard.py
@@ -9,11 +9,7 @@ import math
 import random
 import time
 
-from octoprint_ws281x_led_status.util import (
-    q_poll_milli_sleep,
-    q_poll_sleep,
-    wheel,
-)
+from octoprint_ws281x_led_status.util import q_poll_milli_sleep, wheel
 
 DIRECTIONS = [
     "forward",
@@ -30,98 +26,205 @@ def solid_color(
         strip.setPixelColorRGB(p, *color)
     if show:
         strip.show()
-    if wait:
-        if not q_poll_sleep(0.1, queue):
+
+    while wait:
+        if not q_poll_milli_sleep(0.1, queue):
             return
 
 
 def color_wipe(strip, queue, color, delay, brightness_manager, *args, **kwargs):
-    brightness_manager.reset_brightness()
-    for i in range(strip.numPixels()):
-        strip.setPixelColorRGB(i, *color)
-        strip.show()
-        if not q_poll_milli_sleep(delay, queue):
-            return
-    for i in range(strip.numPixels()):
-        strip.setPixelColorRGB(i, 0, 0, 0)
-        strip.show()
-        if not q_poll_milli_sleep(delay, queue):
-            return
+    while True:
+        brightness_manager.reset_brightness()
+        for i in range(strip.numPixels()):
+            strip.setPixelColorRGB(i, *color)
+            strip.show()
+            if not q_poll_milli_sleep(delay, queue):
+                return
+        for i in range(strip.numPixels()):
+            strip.setPixelColorRGB(i, 0, 0, 0)
+            strip.show()
+            if not q_poll_milli_sleep(delay, queue):
+                return
 
 
 def color_wipe_2(strip, queue, color, delay, brightness_manager, *args, **kwargs):
-    brightness_manager.reset_brightness()
-    for direction in DIRECTIONS:
-        for i in (
-            range(strip.numPixels())
-            if direction == "forward"
-            else reversed(range(strip.numPixels()))
-        ):
-            if direction == "backward":
-                color = (0, 0, 0)
-            strip.setPixelColorRGB(i, *color)
-            strip.show()
-            if not q_poll_milli_sleep(delay, queue):
-                return
+    while True:
+        brightness_manager.reset_brightness()
+        for direction in DIRECTIONS:
+            for i in (
+                range(strip.numPixels())
+                if direction == "forward"
+                else reversed(range(strip.numPixels()))
+            ):
+                if direction == "backward":
+                    color = (0, 0, 0)
+                strip.setPixelColorRGB(i, *color)
+                strip.show()
+                if not q_poll_milli_sleep(delay, queue):
+                    return
 
 
 def simple_pulse(strip, queue, color, delay, brightness_manager, *args, **kwargs):
-    max_brightness = brightness_manager.max_brightness
-    brightness_manager.set_brightness(1)
-    solid_color(
-        strip=strip,
-        queue=queue,
-        color=color,
-        brightness_manager=brightness_manager,
-        wait=False,
-    )
+    while True:
+        max_brightness = brightness_manager.max_brightness
+        brightness_manager.set_brightness(1)
+        solid_color(
+            strip=strip,
+            queue=queue,
+            color=color,
+            brightness_manager=brightness_manager,
+            wait=False,
+        )
 
-    for direction in DIRECTIONS:
-        for b in (
-            range(max_brightness)
-            if direction == "forward"
-            else reversed(range(max_brightness))
-        ):
-            brightness_manager.set_brightness(b)
+        for direction in DIRECTIONS:
+            for b in (
+                range(max_brightness)
+                if direction == "forward"
+                else reversed(range(max_brightness))
+            ):
+                brightness_manager.set_brightness(b)
 
+                if not q_poll_milli_sleep(delay, queue):
+                    return
+
+
+def rainbow(strip, queue, color, delay, brightness_manager, *args, **kwargs):
+    while True:
+        for i in range(256):
+            solid_color(
+                strip=strip,
+                queue=queue,
+                color=wheel(i),
+                delay=delay,
+                wait=False,
+                brightness_manager=brightness_manager,
+            )
             if not q_poll_milli_sleep(delay, queue):
                 return
 
 
-def rainbow(strip, queue, color, delay, brightness_manager, *args, **kwargs):
-    for i in range(256):
-        solid_color(
-            strip=strip,
-            queue=queue,
-            color=wheel(i),
-            delay=delay,
-            wait=False,
-            brightness_manager=brightness_manager,
-        )
-        if not q_poll_milli_sleep(delay, queue):
-            return
-
-
 def rainbow_cycle(strip, queue, color, delay, brightness_manager, *args, **kwargs):
-    brightness_manager.reset_brightness()
-    for j in range(256):
-        for i in range(strip.numPixels()):
-            strip.setPixelColorRGB(
-                i, *wheel((int(i * 256 / strip.numPixels()) + j) & 255)
-            )
-        strip.show()
-        if not q_poll_milli_sleep(delay, queue):
-            return
+    while True:
+        brightness_manager.reset_brightness()
+        for j in range(256):
+            for i in range(strip.numPixels()):
+                strip.setPixelColorRGB(
+                    i, *wheel((int(i * 256 / strip.numPixels()) + j) & 255)
+                )
+            strip.show()
+            if not q_poll_milli_sleep(delay, queue):
+                return
 
 
 def solo_bounce(strip, queue, color, delay, brightness_manager, *args, **kwargs):
-    brightness_manager.reset_brightness()
-    for direction in DIRECTIONS:
-        for i in (
-            range(strip.numPixels())
-            if direction == "forward"
-            else reversed(range(strip.numPixels()))
-        ):
+    while True:
+        brightness_manager.reset_brightness()
+        for direction in DIRECTIONS:
+            for i in (
+                range(strip.numPixels())
+                if direction == "forward"
+                else reversed(range(strip.numPixels()))
+            ):
+                solid_color(
+                    strip=strip,
+                    queue=queue,
+                    color=(0, 0, 0),
+                    brightness_manager=brightness_manager,
+                    wait=False,
+                    show=False,
+                )
+                strip.setPixelColorRGB(i, *color)
+                strip.show()
+                if not q_poll_milli_sleep(delay, queue):
+                    return
+
+
+def bounce(strip, queue, color, delay, brightness_manager, *args, **kwargs):
+    while True:
+        brightness_manager.reset_brightness()
+        red, green, blue, white = color
+        size = 3
+        for direction in DIRECTIONS:
+            for i in (
+                range(0, (strip.numPixels() - size - 2))
+                if direction == "forward"
+                else range((strip.numPixels() - size - 2), 0, -1)
+            ):
+                solid_color(
+                    strip=strip,
+                    queue=queue,
+                    color=(0, 0, 0),
+                    brightness_manager=brightness_manager,
+                    wait=False,
+                    show=False,
+                )
+                strip.setPixelColorRGB(
+                    i,
+                    *(
+                        int(math.floor(red / 10)),
+                        int(math.floor(green / 10)),
+                        int(math.floor(blue / 10)),
+                        int(math.floor(white / 10)),
+                    )
+                )
+                for j in range(1, (size + 1)):
+                    strip.setPixelColorRGB(i + j, *(red, green, blue))
+                strip.setPixelColorRGB(
+                    i + size + 1,
+                    *(
+                        int(math.floor(red / 10)),
+                        int(math.floor(green / 10)),
+                        int(math.floor(blue / 10)),
+                        int(math.floor(white / 10)),
+                    )
+                )
+                strip.show()
+                if not q_poll_milli_sleep(delay, queue):
+                    return
+
+
+def random_single(strip, queue, color, delay, brightness_manager, *args, **kwargs):
+    while True:
+        brightness_manager.reset_brightness()
+        for p in range(strip.numPixels()):
+            strip.setPixelColorRGB(p, *wheel(random.randint(0, 255)))
+        strip.show()
+        while True:
+            strip.setPixelColorRGB(
+                random.randint(0, strip.numPixels()), *wheel(random.randint(0, 255))
+            )
+            strip.show()
+            if not q_poll_milli_sleep(delay, queue):
+                return
+
+
+def blink(strip, queue, color, delay, brightness_manager, *args, **kwargs):
+    while True:
+        for direction in DIRECTIONS:
+            solid_color(
+                strip=strip,
+                queue=queue,
+                color=color if direction == "forward" else (0, 0, 0),
+                brightness_manager=brightness_manager,
+                wait=False,
+            )
+            for _ms in range(int(delay / 2)):
+                if not q_poll_milli_sleep(2, queue):
+                    # We do it this way so we can check the q more often, as for blink
+                    # delay may be high. Otherwise the effect may end up blocking the
+                    # server, when settings are saved, or it shuts down.
+                    return
+
+
+def crossover(strip, queue, color, delay, brightness_manager, *args, **kwargs):
+    while True:
+        brightness_manager.reset_brightness()
+        num_pixels = strip.numPixels()
+        if num_pixels % 2 != 1:
+            # We need an odd number of pixels
+            num_pixels -= 1
+
+        for i in range(num_pixels):
             solid_color(
                 strip=strip,
                 queue=queue,
@@ -131,106 +234,10 @@ def solo_bounce(strip, queue, color, delay, brightness_manager, *args, **kwargs)
                 show=False,
             )
             strip.setPixelColorRGB(i, *color)
+            strip.setPixelColorRGB(num_pixels - 1 - i, *color)
             strip.show()
             if not q_poll_milli_sleep(delay, queue):
                 return
-
-
-def bounce(strip, queue, color, delay, brightness_manager, *args, **kwargs):
-    brightness_manager.reset_brightness()
-    red, green, blue, white = color
-    size = 3
-    for direction in DIRECTIONS:
-        for i in (
-            range(0, (strip.numPixels() - size - 2))
-            if direction == "forward"
-            else range((strip.numPixels() - size - 2), 0, -1)
-        ):
-            solid_color(
-                strip=strip,
-                queue=queue,
-                color=(0, 0, 0),
-                brightness_manager=brightness_manager,
-                wait=False,
-                show=False,
-            )
-            strip.setPixelColorRGB(
-                i,
-                *(
-                    int(math.floor(red / 10)),
-                    int(math.floor(green / 10)),
-                    int(math.floor(blue / 10)),
-                    int(math.floor(white / 10)),
-                )
-            )
-            for j in range(1, (size + 1)):
-                strip.setPixelColorRGB(i + j, *(red, green, blue))
-            strip.setPixelColorRGB(
-                i + size + 1,
-                *(
-                    int(math.floor(red / 10)),
-                    int(math.floor(green / 10)),
-                    int(math.floor(blue / 10)),
-                    int(math.floor(white / 10)),
-                )
-            )
-            strip.show()
-            if not q_poll_milli_sleep(delay, queue):
-                return
-
-
-def random_single(strip, queue, color, delay, brightness_manager, *args, **kwargs):
-    brightness_manager.reset_brightness()
-    for p in range(strip.numPixels()):
-        strip.setPixelColorRGB(p, *wheel(random.randint(0, 255)))
-    strip.show()
-    while True:
-        strip.setPixelColorRGB(
-            random.randint(0, strip.numPixels()), *wheel(random.randint(0, 255))
-        )
-        strip.show()
-        if not q_poll_milli_sleep(delay, queue):
-            return
-
-
-def blink(strip, queue, color, delay, brightness_manager, *args, **kwargs):
-    for direction in DIRECTIONS:
-        solid_color(
-            strip=strip,
-            queue=queue,
-            color=color if direction == "forward" else (0, 0, 0),
-            brightness_manager=brightness_manager,
-            wait=False,
-        )
-        for _ms in range(int(delay / 2)):
-            if not q_poll_milli_sleep(2, queue):
-                # We do it this way so we can check the q more often, as for blink
-                # delay may be high. Otherwise the effect may end up blocking the
-                # server, when settings are saved, or it shuts down.
-                return
-
-
-def crossover(strip, queue, color, delay, brightness_manager, *args, **kwargs):
-    brightness_manager.reset_brightness()
-    num_pixels = strip.numPixels()
-    if num_pixels % 2 != 1:
-        # We need an odd number of pixels
-        num_pixels -= 1
-
-    for i in range(num_pixels):
-        solid_color(
-            strip=strip,
-            queue=queue,
-            color=(0, 0, 0),
-            brightness_manager=brightness_manager,
-            wait=False,
-            show=False,
-        )
-        strip.setPixelColorRGB(i, *color)
-        strip.setPixelColorRGB(num_pixels - 1 - i, *color)
-        strip.show()
-        if not q_poll_milli_sleep(delay, queue):
-            return
 
 
 # Credit to https://www.tweaking4all.com/hardware/arduino/adruino-led-strip-effects/#LEDStripEffectBouncingBalls

octoprint_ws281x_led_status/runner.py
@@ -7,7 +7,17 @@ __copyright__ = "Copyright (c) Charlie Powell 2020-2021 - released under the ter
 
 import logging
 import math
+import multiprocessing
+import threading
 import time
+
+try:
+    # Py3
+    from queue import Queue
+except ImportError:
+    # Py2
+    from Queue import Queue
+
 
 # noinspection PyPackageRequirements
 from octoprint.logging.handlers import CleaningTimedRotatingFileHandler
@@ -16,6 +26,7 @@ from rpi_ws281x import PixelStrip
 from octoprint_ws281x_led_status import constants
 from octoprint_ws281x_led_status.util import (
     apply_color_correction,
+    clear_queue,
     hex_to_rgb,
     int_0_255,
     milli_sleep,
@@ -68,12 +79,15 @@ class EffectRunner:
         self.active_times_state = True
         self.turn_off_timer = None
 
-        self.queue = queue
+        self.queue = queue  # type: multiprocessing.Queue
         try:
-            self.strip = self.start_strip()
+            self.strip = self.start_strip()  # type: PixelStrip
         except StripFailedError:
             self._logger.info("No strip initialised, exiting the effect process.")
             return
+
+        self.effect_queue = Queue()
+        self.effect_thread = None  # type: threading.Thread
 
         self.brightness_manager = BrightnessManager(
             self.strip, self.max_brightness, self.transition_settings
@@ -93,40 +107,40 @@ class EffectRunner:
         msg = self.previous_state
         try:
             while True:
-                if not self.queue.empty():
-                    msg = self.queue.get()  # The ONLY place the queue should be 'got'
+                msg = self.queue.get()
                 if msg:
                     if msg == constants.KILL_MSG:
-                        self.blank_leds()
-                        self._logger.info("Kill message recieved, Bye!")
+                        self.kill()
                         # Exit the process
                         return
                     self.parse_q_msg(msg)  # Effects are run from parse_q_msg
-                    msg = self.previous_state
+
         except KeyboardInterrupt:
-            self.blank_leds()
+            self.kill()
             return
         except Exception as e:
             self._logger.error("Unhandled exception in effect runner process")
             self._logger.exception(e)
             raise
 
+    def kill(self):
+        self.blank_leds()
+        self.stop_effect()
+        self._logger.info("Kill message recieved, all effects stopped. Bye!")
+
     def parse_q_msg(self, msg):
         if msg == "on":
             self.turn_lights_on()
-            return
         elif msg == "off":
             self.turn_lights_off()
-            return
         elif "progress" in msg:
             self.progress_msg(msg)
         elif "M150" in msg:
             self.parse_m150(msg)
-            return
+            self.previous_state = msg
         else:
             self.standard_effect(msg)
-
-        self.previous_state = msg
+            self.previous_state = msg
 
     def turn_lights_on(self):
         if self.turn_off_timer and self.turn_off_timer.is_alive():
@@ -137,7 +151,10 @@ class EffectRunner:
             start_daemon_thread(
                 target=self.brightness_manager.do_fade_in, name="Fade in thread"
             )
-        self._logger.info("On message received, turning on LEDs")
+        self._logger.info(
+            "On message received, turning on LEDs to {}".format(self.previous_state)
+        )
+        self.parse_q_msg(self.previous_state)
 
     def turn_lights_off(self):
         if self.transition_settings["fade"]["enabled"]:
@@ -151,10 +168,16 @@ class EffectRunner:
                 target=self.lights_off,
             )
         else:
-            self.lights_on = False
-        self._logger.info("Off message received, turning off LEDs")
+            self.lights_off()
+
+        self._logger.info(
+            "Off message received, turning off LEDs (fade: {})".format(
+                self.transition_settings["fade"]["enabled"]
+            )
+        )
 
     def lights_off(self):
+        self.blank_leds()
         self.lights_on = False
 
     def progress_msg(self, msg):
@@ -201,16 +224,20 @@ class EffectRunner:
             # Set brightness
             self.brightness_manager.set_brightness(self.previous_m150["brightness"])
             # Set the effects
-            constants.EFFECTS["Solid Color"](
-                strip=self.strip,
-                queue=self.queue,
-                color=apply_color_correction(
-                    self.color_correction,
-                    self.previous_m150["r"],
-                    self.previous_m150["g"],
-                    self.previous_m150["b"],
-                ),
-                brightness_manager=self.brightness_manager,
+            self.run_effect(
+                target=constants.EFFECTS["Solid Color"],
+                kwargs={
+                    "strip": self.strip,
+                    "queue": self.effect_queue,
+                    "color": apply_color_correction(
+                        self.color_correction,
+                        self.previous_m150["r"],
+                        self.previous_m150["g"],
+                        self.previous_m150["b"],
+                    ),
+                    "brightness_manager": self.brightness_manager,
+                },
+                name="Solid Color",
             )
         else:
             self.blank_leds()
@@ -218,18 +245,22 @@ class EffectRunner:
     def progress_effect(self, mode, value):
         effect_settings = self.effect_settings[mode]
         if self.check_times() and self.lights_on:
-            constants.PROGRESS_EFFECTS[effect_settings["effect"]](
-                strip=self.strip,
-                queue=self.queue,
-                brightness_manager=self.brightness_manager,
-                value=int(value),
-                progress_color=apply_color_correction(
-                    self.color_correction, *hex_to_rgb(effect_settings["color"])
-                ),
-                base_color=apply_color_correction(
-                    self.color_correction, *hex_to_rgb(effect_settings["base"])
-                ),
-                reverse=self.reverse,
+            self.run_effect(
+                target=constants.PROGRESS_EFFECTS[effect_settings["effects"]],
+                kwargs={
+                    "strip": self.strip,
+                    "queue": self.effect_queue,
+                    "brightness_manager": self.brightness_manager,
+                    "value": int(value),
+                    "progress_color": apply_color_correction(
+                        self.color_correction, *hex_to_rgb(effect_settings["color"])
+                    ),
+                    "base_color": apply_color_correction(
+                        self.color_correction, *hex_to_rgb(effect_settings["base"])
+                    ),
+                    "reverse": self.reverse,
+                },
+                name=mode,
             )
         else:
             self.blank_leds()
@@ -239,29 +270,55 @@ class EffectRunner:
         if self.previous_state != mode:
             self._logger.debug("Changing effect to {}".format(mode))
 
-        if self.check_times() and self.lights_on:
+        if self.check_times() and self.lights_on and not mode == "blank":
             effect_settings = self.effect_settings[mode]
-            constants.EFFECTS[effect_settings["effect"]](
-                strip=self.strip,
-                queue=self.queue,
-                color=apply_color_correction(
-                    self.color_correction, *hex_to_rgb(effect_settings["color"])
-                ),
-                delay=effect_settings["delay"],
-                brightness_manager=self.brightness_manager,
+            self.run_effect(
+                target=constants.EFFECTS[effect_settings["effect"]],
+                kwargs={
+                    "strip": self.strip,
+                    "queue": self.effect_queue,
+                    "color": apply_color_correction(
+                        self.color_correction, *hex_to_rgb(effect_settings["color"])
+                    ),
+                    "delay": effect_settings["delay"],
+                    "brightness_manager": self.brightness_manager,
+                },
+                name=mode,
             )
         else:
             self.blank_leds()
 
+    def run_effect(self, target, args=(), kwargs=None, name="WS281x Effect"):
+        if kwargs is None:
+            kwargs = {}
+
+        self.stop_effect()
+
+        self.effect_thread = start_daemon_thread(
+            target=target,
+            args=args,
+            kwargs=kwargs,
+            name=name,
+        )
+
+    def stop_effect(self):
+        if self.effect_thread and self.effect_thread.is_alive():
+            self.effect_queue.put(constants.KILL_MSG)
+            self.effect_thread.join()
+            clear_queue(self.effect_queue)
+
     def blank_leds(self):
         """Set LEDs to off, wait 0.1secs to prevent CPU burn"""
-        constants.EFFECTS["Solid Color"](
-            self.strip,
-            self.queue,
-            [0, 0, 0],
-            max_brightness=self.max_brightness,
-            wait=False,
-            brightness_manager=self.brightness_manager,
+        self.run_effect(
+            target=constants.EFFECTS["Solid Color"],
+            kwargs={
+                "strip": self.strip,
+                "queue": self.effect_queue,
+                "color": (0, 0, 0),
+                "brightness_manager": self.brightness_manager,
+                "wait": False,
+            },
+            name="Solid Color",
         )
         if self.queue.empty():
             time.sleep(0.1)

octoprint_ws281x_led_status/util.py
@@ -148,3 +148,8 @@ def start_daemon_timer(target, interval, args=(), kwargs=None):
 
 def int_0_255(value):
     return max(min(int(value), 255), 0)
+
+
+def clear_queue(q):
+    while not q.empty():
+        q.get(False)

commit c0497f42604e58476c6ec82bbd8d757a969dbcca

:bug: Fix high CPU usage on solid colour effects
Regression introduced in 95ea39564fb89e12a7195d211f31fdaeca4a4447

Closes #107

octoprint_ws281x_led_status/effects/standard.py
@@ -28,7 +28,7 @@ def solid_color(
         strip.show()
 
     while wait:
-        if not q_poll_milli_sleep(0.1, queue):
+        if not q_poll_milli_sleep(100, queue):
             return
 
 