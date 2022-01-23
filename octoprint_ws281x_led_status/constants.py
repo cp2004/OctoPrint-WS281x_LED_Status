36a74067e1a5f183c104034bd18ab22cf5ab3728
cp2004 <31997505+cp2004@users.noreply.github.com>
2022-01-23 20:35:02 +0000

    :arrow_up: New documentation branch

--- octoprint_ws281x_led_status/constants.py
+++ octoprint_ws281x_led_status/constants.py
@@ -17,7 +17,7 @@ from octoprint_ws281x_led_status.effects import progress, standard
 # ~~ Documentation
 
 DOCS_URL = "https://cp2004.gitbook.io/ws281x-led-status"
-DOCS_BRANCH = "0.7.x"
+DOCS_BRANCH = "0.8.x"
 DOCS_FULL_LINK = "{}/v/{}/".format(DOCS_URL, DOCS_BRANCH)
 
 PI_REGEX = r"Raspberry Pi (\w*)"
